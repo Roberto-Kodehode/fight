49a8b7e4afa94bcbbf4e7ef19d66529d97ebcd33
Roberto Kodehode <roberto.kodehode@gmail.com>
2023-10-02 12:58:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const canvas = document.getElementById("game")
+const c = canvas.getContext("2d")
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0,0, canvas.width, canvas.height)
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = this.velocity
+    }
+
+    draw() {
+        c.fillStyle = "green";
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+
+    update() {
+        this.draw()
+        this.position.y += 10
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+},
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 100
+},
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+console.log(player)
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = "black"
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
+animate()
